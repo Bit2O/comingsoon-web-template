0796b4dda176f2e9068a1073293a779ed991e596
Bit2O <keshavsharma1706@gmail.com>
2016-02-20 02:47:02 +0530

    new changes

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name comingsoonWebTemplateApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the comingsoonWebTemplateApp
+ */
+angular.module('comingsoonWebTemplateApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name comingsoonWebTemplateApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the comingsoonWebTemplateApp
+ */
+angular.module('comingsoonWebTemplateApp')
+  .controller('MainCtrl', function () {
+    
+});
+
+
+    angular.module('comingsoonWebTemplateApp').directive('countdown',
+        function (Util, $interval) {
+            return {
+                restrict: 'A',
+                scope: { date: '@' },
+                link: function (scope, element) {
+                    var future;
+                    future = new Date(scope.date);
+                    $interval(function () {
+                        var diff;
+                        diff = Math.floor((future.getTime() - new Date().getTime()) / 1000);
+                        return element.text(Util.dhms(diff));
+                    }, 1000);
+                }
+            };
+        }
+    ).factory('Util', [function () {
+            return {
+                dhms: function (t) {
+                    var days, hours, minutes, seconds;
+                    days = Math.floor(t / 86400);
+                    t -= days * 86400;
+                    hours = Math.floor(t / 3600) % 24;
+                    t -= hours * 3600;
+                    minutes = Math.floor(t / 60) % 60;
+                    t -= minutes * 60;
+                    seconds = t % 60;
+                    return [
+                        days + ' days',
+                        hours + ' hours',
+                        minutes + ' minutes',
+                        seconds + ' seconds'
+                    ].join(' ');
+                }
+            };
+        }]);
